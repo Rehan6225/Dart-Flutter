cc51cca5a19c01fad6b434379bff215d83b2d409
Rehan Singh <42702093+Rehan6225@users.noreply.github.com>
2021-06-06 18:44:30 +0530

    Create hello_flutter.dart

---
+++ Dart/hello_flutter.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget{/*Pascals case;class name; inheritance is used by extend property*/
+  Widget build(BuildContext context){//buildcontext:class/type in material.dart
+    return MaterialApp(home:Text('hello'),);
+
+  }
+}
