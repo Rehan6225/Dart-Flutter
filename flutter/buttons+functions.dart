e4aff07ceba7855de48dd88c34d0d677692fdb33
Rehan Singh <42702093+Rehan6225@users.noreply.github.com>
2021-06-07 07:50:28 +0530

    Create buttons+functions.dart

---
+++ flutter/buttons+functions.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+// void main(){
+//   runApp(MyApp());
+// }
+//shorthand for main method
+void main()=>runApp(MyApp());
+
+class MyApp extends StatelessWidget{/*Pascals case;class name; inheritance is used by extend property*/
+  void questionAnswer(){//function that will be called when user clicked the button:line 27
+    print('Answer is chosen');
+  }
+  
+  @override
+  Widget build(BuildContext context){ 
+    //buildcontext:class/type in material.dart
+    var questions=['What\'s yours favourite color?',
+'What\s your favourite animal?'];
+    return MaterialApp
+    (
+      home:Scaffold(
+        appBar: AppBar(
+          title:Text('My First App'),
+          ),
+          //body:Text('This is my default text'),
+          body:Column(children:[
+            Text('The question!'),
+            ElevatedButton(child:Text('Answer 1'),onPressed:questionAnswer),//here button is clicked and void function is called as onPressed takes void funcyion
+            ElevatedButton(child:Text('Answer 1'),onPressed:questionAnswer),
+            ElevatedButton(child:Text('Answer 1'),onPressed:questionAnswer),
+
+          ],),
+        ),
+
+      );
+
+  }
+} 
